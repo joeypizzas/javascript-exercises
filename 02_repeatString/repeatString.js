e0d40a0b1ab823c5d4cdc011f17548401bcc8c0d
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-22 17:04:29 -0500

    Completed JS exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    let addedString = "";
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            addedString = addedString + string;
+        }
+    }
+    return addedString;
+}
 // Do not edit below this line
 module.exports = repeatString;
