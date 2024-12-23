e3b519bdf621b2565cdab1e45221ca273f4552d9
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-23 13:43:58 -0500

    Completed 7/8 tests in exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...itemsRemoved) {
+    for (const item of itemsRemoved) {
+        if (inputArray.indexOf(item) !== -1){
+            inputArray.splice(inputArray.indexOf(item), 1);
+        } 
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
