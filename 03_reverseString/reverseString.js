fbaaf14ef782ca54bb144e3b2a80492ed146d34c
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-23 12:43:27 -0500

    Completed JS exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    const stringArray = stringInput.split('');
+    let reverseStringOutput = '';
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        reverseStringOutput = reverseStringOutput + stringArray[i];
+    }
+    return reverseStringOutput;
 };
 
 // Do not edit below this line
