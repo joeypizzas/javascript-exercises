3aa9deb01711953a881e2a8c741eab03f5768b61
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-24 10:52:15 -0500

    Completed JS exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = 0;
+  celsius = (fahrenheit - 32) * (5/9);
+  let celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = 0;
+  fahrenheit = celsius * (9 / 5) + 32;
+  let roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
