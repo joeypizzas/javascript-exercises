5dba2e6c3a96cf2762c5072cbe817d1b4e95d724
Joe Kaufman <[joe.kaufman527@gmail.com]>
2024-12-24 10:11:55 -0500

    Completed JS exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    if (num1 >= 0 && Number.isInteger(num1) && num2 >= 0 && Number.isInteger(num2)) {
+        if (num2 > num1) {
+            for (let i = num1; i <= num2; i++) {
+                finalSum = finalSum + i;
+            }
+        } else {
+            for (let i = num2; i <=num1; i++) {
+                finalSum = finalSum + i;
+            }
+        }
+        return finalSum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
