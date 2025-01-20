a16e2712c402041f6a554b086f37ff4a5d6755ce
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-01-20 15:04:46 -0500

    Wrote exercise 8 function and have 3/8 tests passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const arr = str.split("");
+    const reversedArr = arr.slice().reverse();
+    for (let i = 0; i < Array.length; i++) {
+        if (arr[i] !== reversedArr[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
