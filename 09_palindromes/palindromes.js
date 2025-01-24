eaca917cff5d6011bc2445addbdd31c20314c02b
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-01-24 08:27:26 -0500

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,15 @@
 const palindromes = function (str) {
     const arr = str.split("");
-    const reversedArr = arr.slice().reverse();
-    for (let i = 0; i < Array.length; i++) {
-        if (arr[i] !== reversedArr[i]) {
+    const lettersOnly = arr.filter(item => /[a-zA-Z0-9]/.test(item));
+    const reversedArr = lettersOnly.slice().reverse();
+    for (let i = 0; i < lettersOnly.length; i++) {
+        if (lettersOnly[i].toLowerCase() !== reversedArr[i].toLowerCase()) {
             return false;
         }
     }
     return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
