082e38a21ee4d1b650dd60c288fa17656d0052ee
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-01-25 15:05:41 -0500

    Completed exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(numInput) {
+    const num = parseInt(numInput);
+    const arr = [1, 1];
+    if (num < 0) {
+        return "OOPS";
+    } else if (num === 0) {
+        return 0;
+    } else {
+        for (let i = 2; i < num; i++) {
+            const newFibNum = arr[i - 1] + arr[i - 2];
+            arr.push(newFibNum);
+        }
+    }
+    return arr[num - 1];
 };
 
 // Do not edit below this line
