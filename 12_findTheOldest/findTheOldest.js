3d03b3758a7229a4e9c725f0217d3d7d9a2100a7
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-01-25 16:52:35 -0500

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let today = new Date();
+    
+    const arrWithAges = arr.map(obj => {
+        if (obj.yearOfBirth && obj.yearOfDeath) {
+            obj.age = obj.yearOfDeath - obj.yearOfBirth;
+        } else {
+            obj.age = today.getFullYear() - obj.yearOfBirth;
+        }
+        return obj;
+    });
+    
+    const arrAgesOnly = arr.map(obj => {
+        if (obj.yearOfBirth && obj.yearOfDeath) {
+            return obj.yearOfDeath - obj.yearOfBirth;
+        } else {
+            return today.getFullYear() - obj.yearOfBirth;
+        }
+    });
 
+    const oldest = arrAgesOnly.reduce((acc, item) => {
+        if (item > acc) {
+            return item;
+        } else {
+            return acc;
+        }
+    }, 0);
+
+    return arrWithAges[arrWithAges.findIndex(obj => obj.age == oldest)];
 };
 
 // Do not edit below this line
